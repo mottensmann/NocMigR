4e3c4633307b065c40a9732ac8e8d01bbbb8cb7c
Meinolf Ottensmann <mottensmann@users.noreply.github.com>
2022-01-05 21:58:21 +0100

    ouput rewritten

--- R/total_duration.R
+++ R/total_duration.R
@@ -5,7 +5,7 @@
 #'
 total_duration <- function(path, format) {
   waves <- list.files(path = path, pattern = format, full.names = T)
-  waves <- waves[!stringr::str_detect(waves, "_extracted.wav")]
+  waves <- waves[!stringr::str_detect(waves, paste0("_extracted.", format,""))]
   waves <- waves[!stringr::str_detect(waves, "merged.events.wav")]
 
   duration <- sapply(waves, tuneR::readWave, header = T)
@@ -17,11 +17,11 @@ total_duration <- function(path, format) {
 
   sample_rate <- tuneR::readWave(waves[1], header = TRUE)$sample.rate
 
-data.frame(
-  sample_rate = sample_rate,
-  secounds = duration,
-  hours = duration/3600)
-
+  data.frame(sample_rate = paste(sample_rate, "Hz"),
+             duration = dplyr::case_when(
+               duration < 60 ~ paste(round(duration, 2), "seconds"),
+               duration > 60 & duration < 3600 ~ paste(round(duration/60, 2), "minutes"),
+               duration > 3600 ~ paste(round(duration/3600, 2), "hours")))
 }
 #
 # path <- "I:/NocMig/AudioMoth/Patthorst 20211211 - 20211217/"
