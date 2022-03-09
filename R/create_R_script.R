2ac160a370b54796022f246515c3bb5820e9aeca
Meinolf Ottensmann <mottensmann@users.noreply.github.com>
2022-03-09 17:26:16 +0100

    update

--- R/create_R_script.R
+++ R/create_R_script.R
@@ -11,15 +11,16 @@ create_R_script <- function(path, open = TRUE) {
   cat(paste0('
 df <- batch_process(path = "', path,'",
                     format = "WAV",
-                    recorder = "AudioMoth",
+                    recorder = "Olympus LS-3",
                     target = td_presets("NocMig"),
                     SNR = 6,
                     downsample = 44100,
                     mono = TRUE,
+                    rename = TRUE,
                     steps = 1:6,
-                    max.events = 400)
+                    max.events = 9999)
      '))
-  cat(paste0('cleanup_batch_process(path = "', path,'")'))
+cat(paste0('cleanup_batch_process(path = "', path,'")'))
   sink()
-  if(open == TRUE) file.show(file.path(path, "R_code.R"))
+  if (open == TRUE) file.show(file.path(path, "R_code.R"))
 }
